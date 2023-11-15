6c2ce249c008b624f1a0ab5ffad05d9c5619c7d6
Francokab <francoiskabiry@yahoo.fr>
2023-11-15 11:30:54 +0100

    added a files to include all

---
+++ include/AllFiles.h
@@ -0,0 +1,17 @@
+#ifndef ALLFILES_H
+#define ALLFILES_H
+
+#pragma once
+
+#include "Objet.h"
+#include "Arme.h"
+#include "Armure.h"
+#include "Consommable.h"
+#include "Magie.h"
+
+#include "personnage.h"
+#include "monstre.h"
+#include "civil.h"
+#include "joueurs.h"
+
+#endif
