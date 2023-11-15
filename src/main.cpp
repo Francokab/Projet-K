7af36a96ce7eb329db5da6a32b1083b7c4349aad
Francokab <francoiskabiry@yahoo.fr>
2023-11-15 10:17:06 +0100

    Added Cmake and the text

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <bitset>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+  cout << "Hello World" << "\n";
+
+  return 0;
+}
