3933de6d7d5423cb1bee501b5f100ff002b294dd
Ludovic <ludovicsenez861@gmail.com>
2023-12-03 13:30:03 +0100

    Added special character to cout

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,21 @@
 #include <vector>
 #include <list>
 
+//Ces includes sont présents pour cout les caractères spéciaux (types é, à, è).
+//Cela ne devrait pas marcher sur un linux.
+#include <cstdio>
+#include <windows.h>
+
+#pragma execution_character_set( "utf-8" )
+
 using namespace std;
 
 #include "AllFiles.h"
 
 int main()
 {
-
-  cout << "Hello World"
-       << "\n";
+  //Pour les caractères spéciaux
+  SetConsoleOutputCP( 65001 );
 
   Jeu jeu;
 
